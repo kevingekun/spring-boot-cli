cd3d07029725593b26e50ea65b8032eae6eaa345
kevinge <kevingekun@gmail.com>
2025-07-29 15:33:10 +0800

    删除无用方法

--- src/main/java/com/macro/mall/tiny/modules/futu/controller/StocksBaseController.java
+++ src/main/java/com/macro/mall/tiny/modules/futu/controller/StocksBaseController.java
@@ -1,12 +1,15 @@
 package com.macro.mall.tiny.modules.futu.controller;
 
 import com.macro.mall.tiny.common.api.CommonResult;
+import com.macro.mall.tiny.modules.futu.model.StocksBase;
+import com.macro.mall.tiny.modules.futu.service.StocksBaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.macro.mall.tiny.modules.futu.service.StocksBaseService;
-import com.macro.mall.tiny.modules.futu.model.StocksBase;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
-import org.springframework.web.bind.annotation.*;
 
 
 /**
@@ -24,48 +27,9 @@ public class StocksBaseController {
     @Autowired
     public StocksBaseService stocksBaseService;
 
-    @RequestMapping(value = "/create", method = RequestMethod.POST)
-    @ResponseBody
-    public CommonResult create(@RequestBody StocksBase stocksBase) {
-        boolean success = stocksBaseService.save(stocksBase);
-        if (success) {
-            return CommonResult.success(null);
-        }
-        return CommonResult.failed();
-    }
-
-    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
-    @ResponseBody
-    public CommonResult update(@PathVariable Long id, @RequestBody StocksBase stocksBase) {
-        stocksBase.setId(id.intValue());
-        boolean success = stocksBaseService.updateById(stocksBase);
-        if (success) {
-            return CommonResult.success(null);
-        }
-        return CommonResult.failed();
-    }
-
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
-    @ResponseBody
-    public CommonResult delete(@PathVariable Long id) {
-        boolean success = stocksBaseService.removeById(id);
-        if (success) {
-            return CommonResult.success(null);
-        }
-        return CommonResult.failed();
-    }
-
-    @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    @ResponseBody
-    public CommonResult deleteBatch(@RequestParam("ids") List<Long> ids) {
-        boolean success = stocksBaseService.removeByIds(ids);
-        if (success) {
-            return CommonResult.success(null);
-        }
-        return CommonResult.failed();
-    }
-
-
+    /**
+     * 获取所有股票基础信息
+     */
     @RequestMapping(value = "/listAll", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<List<StocksBase>> listAll() {
